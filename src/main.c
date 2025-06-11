c096e7522acefcd63338156048e97812d486f1f1
evanlin917 <evanlin1724@gmail.com>
2025-06-10 23:03:57 -0400

    feat: Implement echo builtin command to print to stdout

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
+  int status = 0;
 
   while (1) {
     printf("$ ");
@@ -11,24 +12,62 @@ int main() {
     // Wait for user input
     const size_t bufSize = 512;
     char input[bufSize];
-    char* userInput = fgets(input, bufSize, stdin);
+    char* shellInput = fgets(input, bufSize, stdin);
 
-    if (userInput == NULL) {
+    if (shellInput == NULL) {
       break;
     }
 
     // Remove trailing newline only if it exists
     size_t inputLen = strlen(input);
     if (inputLen > 0 && input[inputLen - 1] == '\n') {
-      input[strlen(input) - 1] = '\0';
+      input[inputLen - 1] = '\0';
     }
 
-    if (strcmp(input, "exit 0") == 0) {
-      break;
+    // Preserve input for error reporting
+    char inputCopy[bufSize];
+    strcpy(inputCopy, input);
+
+    // Extract command and args from input given to shell
+    char* args = strchr(input, ' ');
+    if (args != NULL) {
+      *args = '\0';
+      args++;
+
+      while (*args == ' ') {
+        args++;
+      }
+    }
+
+    char* command = input;
+    if (command == NULL) {
+      continue;
     }
     
-    printf("%s: command not found\n", input);
+    if (strcmp(command, "exit") == 0) {
+      status = handle_exit_cmd(args);
+      break;
+    } else if (strcmp(command, "echo") == 0) {
+      handle_echo_cmd(args);
+    } else {
+      printf("%s: command not found\n", inputCopy);
+    }
   }
 
-  return 0;
+  return status;
+}
+
+// Helper function to handle `echo` commands
+void handle_echo_cmd(const char *args) {
+  if (args != NULL) {
+    printf("%s\n", args);
+  }
+  else {
+    printf("\n");
+  }
+}
+
+// Helper function to handle `exit` commands
+int handle_exit_cmd(const char *args) {
+  return (args != NULL) ? atoi(args) : 0;
 }
