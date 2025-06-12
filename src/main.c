4d2d7e36fa067f994374b166adce1a730884fcf2
evanlin917 <evanlin1724@gmail.com>
2025-06-12 15:07:16 -0400

    fix: Address double free issue

--- src/main.c
+++ src/main.c
@@ -296,7 +296,7 @@ int main() {
       char* pwd = handle_pwd_cmd();
       if (pwd != NULL) {
         printf("%s\n", pwd);
-        free(pwd);
+        // free(pwd);
       } else {
         printf("pwd could not retrieve current working directory\n");
       }
